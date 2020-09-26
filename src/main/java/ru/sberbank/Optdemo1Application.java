3a12fd30d2f027374d2834bb6b80ff5349dc0e27
Дмитрий Оторов <otorov_dmitriy@iMac-Dmitrij.local>
2020-09-26 22:56:55 +0300

    hometask-1

--- src/main/java/ru/sberbank/Optdemo1Application.java
+++ src/main/java/ru/sberbank/Optdemo1Application.java
@@ -1,14 +1,26 @@
 package ru.sberbank;
 
+import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import ru.sberbank.lab1.Lab1Controller;
 
 @SpringBootApplication
+@EnableScheduling
 public class Optdemo1Application {
+    @Autowired
+    Lab1Controller lab1Controller;
 
     public static void main(String[] args) {
         SpringApplication.run(Optdemo1Application.class, args);
+    }
+
+    // Данный метод позволяет предварительно получить данные, чтобы была возможность закэшировать их.
+    @Scheduled(fixedDelay = Long.MAX_VALUE)
+    public void initTemperatureData() throws JSONException {
+        lab1Controller.getTemperatureForLastDays(5);
     }
 }
