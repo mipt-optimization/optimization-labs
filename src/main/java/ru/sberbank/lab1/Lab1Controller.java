f9d334668110d4924423f92682c6cf97575306c7
Nikitos-U <uhov.na@phystech.edu>
2020-09-29 22:41:30 +0300

    added caching, warm up, refactored + CHANGED COORDS TO YOSHKAR-OLA

--- src/main/java/ru/sberbank/lab1/Lab1Controller.java
+++ src/main/java/ru/sberbank/lab1/Lab1Controller.java
@@ -6,6 +6,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,6 +31,7 @@ import static java.util.Collections.emptyList;
 public class Lab1Controller {
 
     private static final String URL = "http://export.rbc.ru/free/selt.0/free.fcgi?period=DAILY&tickers=USD000000TOD&separator=TAB&data_format=BROWSER";
+    private final Map<String, Double> dateTemperatureCache = new HashMap<>();
 
     @GetMapping("/quotes")
     public List<Quote> quotes(@RequestParam("days") int days) throws ExecutionException, InterruptedException, ParseException {
@@ -42,8 +45,8 @@ public class Lab1Controller {
 
         Map<String, Double> maxMap = new HashMap<>();
 
-        for (int i = 0; i < lines.length; i++) {
-            String[] line = lines[i].split("\t");
+        for (String s : lines) { //changed for to enhanced for
+            String[] line = s.split("\t");
             Date date = new SimpleDateFormat("yyyy-MM-dd").parse(line[1]);
             String year = line[1].split("-")[0];
             String month = line[1].split("-")[1];
@@ -110,11 +113,18 @@ public class Lab1Controller {
         return quotes;
     }
 
+    //    added everyday warm up
+    @Async
+    @Scheduled(cron = "0 0 12 * * ?")
+    void everydayWarmUp() {
+        getWeatherForPeriod(10);
+    }
+
     @GetMapping("/weather")
     public List<Double> getWeatherForPeriod(Integer days) {
         try {
             return getTemperatureForLastDays(days);
-        } catch (JSONException e) {
+        } catch (JSONException ignored) { //refactored catch
         }
 
         return emptyList();
@@ -122,27 +132,34 @@ public class Lab1Controller {
 
     public List<Double> getTemperatureForLastDays(int days) throws JSONException {
         List<Double> temps = new ArrayList<>();
+        long currentDayInSec = Calendar.getInstance().getTimeInMillis() / 1000; // moved const from loop
+        long oneDayInSec = 86400L; //moved const from loop, removed unnecessary math
 
         for (int i = 0; i < days; i++) {
-            Long currentDayInSec = Calendar.getInstance().getTimeInMillis() / 1000;
-            Long oneDayInSec = 24 * 60 * 60L;
-            Long curDateSec = currentDayInSec - i * oneDayInSec;
-            Double curTemp = getTemperatureFromInfo(curDateSec.toString());
-            temps.add(curTemp);
+            long curDateSec = currentDayInSec - i * oneDayInSec;
+            long curDay = curDateSec / oneDayInSec;
+
+            if (!dateTemperatureCache.containsKey(Long.toString(curDay))) { //added caching
+                Double curTemp = getTemperatureFromInfo(Long.toString(curDateSec));
+                temps.add(curTemp);
+                dateTemperatureCache.put(Long.toString(curDay), curTemp);
+            } else {
+                temps.add(dateTemperatureCache.get(Long.toString(curDay)));
+            }
         }
 
         return temps;
     }
 
     public String getTodayWeather(String date) {
-        String obligatoryForecastStart = "https://api.darksky.net/forecast/ac1830efeff59c748d212052f27d49aa/";
-        String LAcoordinates = "34.053044,-118.243750,";
+        String obligatoryForecastStart = "https://api.darksky.net/forecast/3ce5ca6c6c64befaa69dd9cf05b939db/";
+        String YoCityCoordinates = "56.634407,47.899878,"; //Changer coords to coords of the best city on Earth!!!!!!!
         String exclude = "exclude=daily";
 
         RestTemplate restTemplate = new RestTemplate();
-        String fooResourceUrl = obligatoryForecastStart + LAcoordinates + date + "?" + exclude;
+        StringBuilder fooResourceUrl = new StringBuilder(obligatoryForecastStart + YoCityCoordinates + date + "?" + exclude); //string -> StringBuilder
         System.out.println(fooResourceUrl);
-        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl.toString(), String.class);
         String info = response.getBody();
         System.out.println(info);
         return info;
@@ -150,17 +167,15 @@ public class Lab1Controller {
 
     public Double getTemperatureFromInfo(String date) throws JSONException {
         String info = getTodayWeather(date);
-        Double curTemp = getTemperature(info);
-        return curTemp;
+        return getTemperature(info); //inlined var
     }
 
     public Double getTemperature(String info) throws JSONException {
         JSONObject json = new JSONObject(info);
         String hourly = json.getString("hourly");
         JSONArray data = new JSONObject(hourly).getJSONArray("data");
-        Double temp = new JSONObject(data.get(0).toString()).getDouble("temperature");
 
-        return temp;
+        return new JSONObject(data.get(0).toString()).getDouble("temperature"); //inlined var
     }
 }
 
