adad76ae9ee71b47b17127c1c812c7c03b9b9bdb
UkhovArty <uhov.aa@phystech.edu>
2020-09-29 22:48:08 +0300

    Сделал кэширование температуры за уже известные дни, прикрутил 'прогрев', убрал ненужные переменные и вынес константы из циклов

--- src/main/java/ru/sberbank/lab1/Lab1Controller.java
+++ src/main/java/ru/sberbank/lab1/Lab1Controller.java
@@ -6,6 +6,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +22,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 
 import static java.util.Collections.emptyList;
@@ -29,7 +32,8 @@ import static java.util.Collections.emptyList;
 public class Lab1Controller {
 
     private static final String URL = "http://export.rbc.ru/free/selt.0/free.fcgi?period=DAILY&tickers=USD000000TOD&separator=TAB&data_format=BROWSER";
-
+    //топорно прикрутил кэш
+    private final Map<Long, Double> cache = new ConcurrentHashMap<>();
     @GetMapping("/quotes")
     public List<Quote> quotes(@RequestParam("days") int days) throws ExecutionException, InterruptedException, ParseException {
         AsyncHttpClient client = AsyncHttpClientFactory.create(new AsyncHttpClientFactory.AsyncHttpClientConfig());
@@ -114,28 +118,35 @@ public class Lab1Controller {
     public List<Double> getWeatherForPeriod(Integer days) {
         try {
             return getTemperatureForLastDays(days);
-        } catch (JSONException e) {
+        } catch (JSONException ignored) {
         }
 
         return emptyList();
     }
 
-    public List<Double> getTemperatureForLastDays(int days) throws JSONException {
+    private List<Double> getTemperatureForLastDays(int days) throws JSONException {
         List<Double> temps = new ArrayList<>();
+        //Вынес константы из цикла
+        long currentDayInSec = Calendar.getInstance().getTimeInMillis() / 1000;
+        long oneDayInSec = 86400L;
 
         for (int i = 0; i < days; i++) {
-            Long currentDayInSec = Calendar.getInstance().getTimeInMillis() / 1000;
-            Long oneDayInSec = 24 * 60 * 60L;
-            Long curDateSec = currentDayInSec - i * oneDayInSec;
-            Double curTemp = getTemperatureFromInfo(curDateSec.toString());
-            temps.add(curTemp);
+            long curDateSec = currentDayInSec - i * oneDayInSec;
+            long date = curDateSec / oneDayInSec;
+            // берем из кэша если там есть значение
+            if (!cache.containsKey(date)) {
+                Double temp = getTemperatureFromInfo(Long.toString(curDateSec));
+                temps.add(temp);
+                cache.put(date, temp);
+            }
+            temps.add(cache.get(date));
         }
 
         return temps;
     }
 
-    public String getTodayWeather(String date) {
-        String obligatoryForecastStart = "https://api.darksky.net/forecast/ac1830efeff59c748d212052f27d49aa/";
+    private String getTodayWeather(String date) {
+        String obligatoryForecastStart = "https://api.darksky.net/forecast/7680422f32effb534f62f1283f0c38be/";
         String LAcoordinates = "34.053044,-118.243750,";
         String exclude = "exclude=daily";
 
@@ -148,19 +159,24 @@ public class Lab1Controller {
         return info;
     }
 
-    public Double getTemperatureFromInfo(String date) throws JSONException {
-        String info = getTodayWeather(date);
-        Double curTemp = getTemperature(info);
-        return curTemp;
+    //Убрал ненужное использование переменной
+    private Double getTemperatureFromInfo(String date) throws JSONException {
+        return getTemperature(getTodayWeather(date));
     }
 
-    public Double getTemperature(String info) throws JSONException {
+    //Убрал ненужное использование переменной
+    private Double getTemperature(String info) throws JSONException {
         JSONObject json = new JSONObject(info);
         String hourly = json.getString("hourly");
         JSONArray data = new JSONObject(hourly).getJSONArray("data");
-        Double temp = new JSONObject(data.get(0).toString()).getDouble("temperature");
+        return new JSONObject(data.get(0).toString()).getDouble("temperature");
+    }
 
-        return temp;
+    // 'Прогревает' кэш каждый день в 10 утра на 5 дней вперед
+    @Async
+    @Scheduled(cron = "0 45 22 * * ?")
+    public void warmUp() {
+        getWeatherForPeriod(5);
     }
 }
 
