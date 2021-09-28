dc9277c94d6befdcebe20759e9f9715784133735
eakovtun <EAbKovtun@sberbank.ru>
2021-09-28 20:51:03 +0300

    All Kovtun EA

--- src/main/java/ru/sberbank/lab1/Lab1Controller.java
+++ src/main/java/ru/sberbank/lab1/Lab1Controller.java
@@ -1,166 +1,75 @@
 package ru.sberbank.lab1;
 
-import org.asynchttpclient.AsyncHttpClient;
-import org.asynchttpclient.Response;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
+import java.time.LocalDate;
+import java.util.*;
 
 import static java.util.Collections.emptyList;
 
 @RestController
 @RequestMapping("/lab1")
 public class Lab1Controller {
+    //TODO thread-safe, поэтому будем юзать один экземпляр, а не создавать на каждый запрос новый
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final TemperatureCache temperatureCache;
 
-    private static final String URL = "http://export.rbc.ru/free/selt.0/free.fcgi?period=DAILY&tickers=USD000000TOD&separator=TAB&data_format=BROWSER";
+    public Lab1Controller(TemperatureCache temperatureCache) { this.temperatureCache = temperatureCache; }
 
-    @GetMapping("/quotes")
-    public List<Quote> quotes(@RequestParam("days") int days) throws ExecutionException, InterruptedException, ParseException {
-        AsyncHttpClient client = AsyncHttpClientFactory.create(new AsyncHttpClientFactory.AsyncHttpClientConfig());
-        Response response = client.prepareGet(URL + "&lastdays=" + days).execute().get();
-
-        String body = response.getResponseBody();
-        String[] lines = body.split("\n");
-
-        List<Quote> quotes = new ArrayList<>();
-
-        Map<String, Double> maxMap = new HashMap<>();
-
-        for (int i = 0; i < lines.length; i++) {
-            String[] line = lines[i].split("\t");
-            Date date = new SimpleDateFormat("yyyy-MM-dd").parse(line[1]);
-            String year = line[1].split("-")[0];
-            String month = line[1].split("-")[1];
-            String monthYear = year + month;
-            Double high = Double.parseDouble(line[3]);
-
-            Double maxYear = maxMap.get(year);
-            if (maxYear == null || maxYear < high) {
-                maxMap.put(year, high);
-                if (maxYear != null) {
-                    List<Quote> newQuotes = new ArrayList<>();
-                    for (Quote oldQuote : quotes) {
-                        Calendar cal = Calendar.getInstance();
-                        cal.setTime(oldQuote.getDate());
-                        int oldYear = cal.get(Calendar.YEAR);
-                        if (oldYear == Integer.parseInt(year)) {
-                            if (oldQuote.getMaxInYear() < high) {
-                                Quote newQuote = oldQuote.setMaxInYear(high);
-                                newQuotes.add(newQuote);
-                            } else {
-                                newQuotes.add(oldQuote);
-                            }
-                        }
-                    }
-                    quotes.clear();
-                    quotes.addAll(newQuotes);
-                }
-            }
-
-            Double maxMonth = maxMap.get(monthYear);
-            if (maxMonth == null || maxMonth < high) {
-                maxMap.put(monthYear, high);
-                if (maxMonth != null) {
-                    List<Quote> newQuotes = new ArrayList<>();
-                    for (Quote oldQuote : quotes) {
-                        Calendar cal = Calendar.getInstance();
-                        cal.setTime(oldQuote.getDate());
-                        int oldYear = cal.get(Calendar.YEAR);
-                        int oldMonth = cal.get(Calendar.MONTH);
-                        if (oldYear == Integer.parseInt(year) && oldMonth == Integer.parseInt(month)) {
-                            if (oldQuote.getMaxInMonth() < high) {
-                                Quote newQuote = oldQuote.setMaxInMonth(high);
-                                quotes.remove(oldQuote);
-                                quotes.add(newQuote);
-                            }
-                        }
-                    }
-                }
-            }
-
-            Quote quote = new Quote(line[0],
-                    new SimpleDateFormat("yyyy-MM-dd").parse(line[1]),
-                    Double.parseDouble(line[2]),
-                    Double.parseDouble(line[3]),
-                    Double.parseDouble(line[4]),
-                    Double.parseDouble(line[5]),
-                    Long.parseLong(line[6]),
-                    Double.parseDouble(line[7]));
-            quote = quote.setMaxInMonth(maxMap.get(monthYear));
-            quote = quote.setMaxInYear(maxMap.get(year));
-
-            quotes.add(quote);
-        }
-        return quotes;
+    @EventListener(ApplicationReadyEvent.class)
+    public void heatingCache() {
+        try { getTemperatureForLastDays(300); } catch (JSONException e) { System.out.println("Cache not heating"); }
+        System.out.println("Cache heating");
     }
 
     @GetMapping("/weather")
-    public List<Double> getWeatherForPeriod(Integer days) {
-        try {
-            return getTemperatureForLastDays(days);
-        } catch (JSONException e) {
-        }
-
-        return emptyList();
+    public List<Double> getWeatherForPeriod(int days) {
+        try { return getTemperatureForLastDays(days); } catch (JSONException ignore) { return emptyList(); }
     }
 
     public List<Double> getTemperatureForLastDays(int days) throws JSONException {
-        List<Double> temps = new ArrayList<>();
+        List<Double> temperatureFromCache;
+        synchronized (temperatureCache) { temperatureFromCache = temperatureCache.getTemperature(days); }
+        if (temperatureFromCache != null) return temperatureFromCache;
 
-        for (int i = 0; i < days; i++) {
-            Long currentDayInSec = Calendar.getInstance().getTimeInMillis() / 1000;
-            Long oneDayInSec = 24 * 60 * 60L;
-            Long curDateSec = currentDayInSec - i * oneDayInSec;
-            Double curTemp = getTemperatureFromInfo(curDateSec.toString());
-            temps.add(curTemp);
-        }
+        //TODO инициализацию времени вынес из цикла + получаем время начала дня, чтобы в кэше было меньше промахов
+        long oneDayInSec = 24 * 60 * 60L;
+        long currentDayInSec = LocalDate.now().toEpochDay() * oneDayInSec;
 
+        //TODO capacity массива сразу под нужный размер (избегаем выделения размера на этапе заполнения массива)
+        List<Double> temps = new ArrayList<>(days);
+        for (int i = 0; i < days; i++) temps.add(getTemperatureFromInfo(currentDayInSec - i * oneDayInSec));
+
+        synchronized (temperatureCache) { temperatureCache.syncCache(temps); }
         return temps;
     }
 
-    public String getTodayWeather(String date) {
-        String obligatoryForecastStart = "https://api.darksky.net/forecast/ac1830efeff59c748d212052f27d49aa/";
+    //TODO тип параметра сразу в лонг
+    public String getTodayWeather(long date) {
+        String obligatoryForecastStart = "https://api.darksky.net/forecast/7ba6164198e89cb2e6b2454d90e7b41d/";
         String LAcoordinates = "34.053044,-118.243750,";
-        String exclude = "exclude=daily";
-
-        RestTemplate restTemplate = new RestTemplate();
+        //TODO Добавил еще эксклюдов для увеличения производительности сервера и снижения размера сообщения от сервера
+        String exclude = "exclude=currently,minutely,daily,alerts,flags";
         String fooResourceUrl = obligatoryForecastStart + LAcoordinates + date + "?" + exclude;
-        System.out.println(fooResourceUrl);
-        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
-        String info = response.getBody();
-        System.out.println(info);
-        return info;
+        return restTemplate.getForEntity(fooResourceUrl, String.class).getBody();
     }
 
-    public Double getTemperatureFromInfo(String date) throws JSONException {
+    //TODO тип параметра сразу в лонг
+    public Double getTemperatureFromInfo(long date) throws JSONException {
         String info = getTodayWeather(date);
-        Double curTemp = getTemperature(info);
-        return curTemp;
+        return getTemperature(info);
     }
 
     public Double getTemperature(String info) throws JSONException {
-        JSONObject json = new JSONObject(info);
-        String hourly = json.getString("hourly");
-        JSONArray data = new JSONObject(hourly).getJSONArray("data");
-        Double temp = new JSONObject(data.get(0).toString()).getDouble("temperature");
-
-        return temp;
+        //TODO в одну строку, без создания промежуточной строки
+        return new JSONObject(info).getJSONObject("hourly").getJSONArray("data").getJSONObject(0).getDouble("temperature");
     }
 }
 
